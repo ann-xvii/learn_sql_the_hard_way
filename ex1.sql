ca6d099959a6a4a434c81a0714d649b121c925f0
An.nA <anna.git3@gmail.com>
2015-03-19 21:02:49 -0700

    ex1

---
+++ ex1.sql
@@ -0,0 +1,9 @@
+CREATE TABLE person (
+	id INTEGER PRIMARY KEY,
+	first_name TEXT,
+	last_name TEXT,
+	age INTEGER
+);
+
+-- an ID column is used to exactly identify each row
+-- in addition, specify that this column is primary key 
